f2b851189f76cb330985774ed176784aa89d77f4
naziya <121482938+missnaziya@users.noreply.github.com>
2025-09-10 11:48:15 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import FCMTokenHandler from "./notification/FCMTokenHandler";
+import Routes from "./utils/Routes";
+
+function App() {
+  return (
+    <>   <FCMTokenHandler />
+      <Routes />
+    </>
+  );
+}
+
+export default App;
